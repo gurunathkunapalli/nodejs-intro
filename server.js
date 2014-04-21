44d81b02d4f0573741d363d822e8ed1cb8993889
Andre Behrens <mrandre@gmail.com>
2014-04-21 18:55:33 -0400

    Re-implemented the request using the "request" module.

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 /*
  *
- * Implemented a new path, /data.xml that proxies a request for the homepage xml file.
+ * Re-implemented, with a new dependency, request.
  *
- * This looks weird and complicated and it is.
- * 
- * Notice that we have to parse the url we want into host and path using the built-in url module.
+ * MUCH simpler. Once more, we have a complicated core method that focuses on completeness, simplified with a layered module implementing the underlying techniques more simply.
  *
- * We construct a header out of these values.
- *
- * Notice the .on events system of managing data connections. Notice the chunking of data. This makes the app faster, but much harder to understand.
+ * No chunks, no parsed urls, just a request piped to the response.
  * 
  */
 
 
-var http = require('http');
-var url = require('url');
+var request = require("request");
 var express = require("express");
 var app = express();
 
@@ -23,25 +18,9 @@ app.get('/', function(req, res) {
 });
 
 app.get('/data.xml', function(req, res) {
+  var requestUrl = "http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml";
   res.setHeader("content-type", "text/xml");
-  var requestUrl = url.parse("http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml");
-  var opts = {
-    "host": requestUrl.host,
-    "path": requestUrl.path,
-    "headers": {
-      "Content-Type": "text/xml"
-    }
-  };
-  console.log(JSON.stringify(opts));
-  http.get(opts, function(getRes) {
-    getRes.setEncoding("utf8");
-    var resData = "";
-    getRes.on('data', function(chunk) {
-      return resData += chunk;
-    }).on("end", function() {
-      res.send(resData);
-    });
-  });
+  request(requestUrl).pipe(res);
 });
 
 console.log("Server running at http://127.0.0.1:1337/");
